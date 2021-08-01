36737f232d3b7cfa126fb68ecbdba14f043edf95
ColinAuyeung1 <87677688+ColinAuyeung1@users.noreply.github.com>
2021-08-01 17:21:05 -0400

    Add files via upload

---
+++ ProgrammingA1.py
@@ -0,0 +1,10 @@
+l = int(input("enter 1st number"))
+w = int(input("enter 2nd number"))
+h = int(input("enter 3rd number"))
+a = l * w *h
+print(a)
+primer = 200
+paint = 350
+print(a / primer)
+print(a / paint)
+
